95507f0ebcc3e669ef40732ab7d95b292b99ecfe
NabokovSG <nabokovsg@yandex.ru>
2023-11-24 15:54:41 +0300

    add  findAllByReportingDocumentIds

--- DataLaboratoryCenter/templates/src/main/java/ru/nabokovsg/templates/repository/HeaderTemplateRepository.java
+++ DataLaboratoryCenter/templates/src/main/java/ru/nabokovsg/templates/repository/HeaderTemplateRepository.java
@@ -1,9 +1,18 @@
 package ru.nabokovsg.templates.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import ru.nabokovsg.templates.models.HeaderTemplate;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
 
 public interface HeaderTemplateRepository extends JpaRepository<HeaderTemplate, Long> {
 
-    HeaderTemplate findByReportingDocumentId(Long reportingDocumentId);
+    @Query("select h from HeaderTemplate h where h.reportingDocumentId in :reportingDocumentIds")
+    Set<HeaderTemplate> findAllByReportingDocumentIds(@Param("reportingDocumentIds") List<Long> reportingDocumentIds);
+
+    Optional<HeaderTemplate> findByReportingDocumentId(Long reportingDocumentId);
 }
