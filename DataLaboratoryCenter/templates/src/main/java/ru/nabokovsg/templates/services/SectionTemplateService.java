16febe363815e4927044ace41a99f881d2e8da49
NabokovSG <nabokovsg@yandex.ru>
2023-11-24 15:59:24 +0300

    add update(), get(). create()

--- DataLaboratoryCenter/templates/src/main/java/ru/nabokovsg/templates/services/SectionTemplateService.java
+++ DataLaboratoryCenter/templates/src/main/java/ru/nabokovsg/templates/services/SectionTemplateService.java
@@ -1,9 +1,20 @@
 package ru.nabokovsg.templates.services;
 
-import ru.nabokovsg.templates.dto.report.ReportTemplateDto;
-import ru.nabokovsg.templates.dto.sections.NewReportSectionTemplateDto;
+import ru.nabokovsg.templates.dto.sections.SectionTemplateDto;
+import ru.nabokovsg.templates.dto.sections.NewSectionTemplateDto;
+import ru.nabokovsg.templates.dto.sections.UpdateSectionTemplateDto;
+import ru.nabokovsg.templates.models.SectionTemplate;
+
+import java.util.List;
+import java.util.Set;
 
 public interface SectionTemplateService {
 
-    ReportTemplateDto save(NewReportSectionTemplateDto sectionTemplateDto);
+    List<SectionTemplateDto> save(NewSectionTemplateDto sectionsDto);
+
+    List<SectionTemplateDto> update(UpdateSectionTemplateDto sectionsDto);
+
+    List<SectionTemplateDto> get(Long reportingDocumentId);
+
+    Set<SectionTemplate> create(Long reportingDocumentId);
 }
