23bfa255b1cc2f6dd987d612f7ea1d79896a0245
NabokovSG <nabokovsg@yandex.ru>
2023-11-24 15:58:00 +0300

    add update(), get()

--- DataLaboratoryCenter/templates/src/main/java/ru/nabokovsg/templates/controllers/SectionTemplateController.java
+++ DataLaboratoryCenter/templates/src/main/java/ru/nabokovsg/templates/controllers/SectionTemplateController.java
@@ -7,37 +7,51 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import ru.nabokovsg.templates.dto.report.ReportTemplateDto;
-import ru.nabokovsg.templates.dto.sections.NewReportSectionTemplateDto;
+import org.springframework.web.bind.annotation.*;
+import ru.nabokovsg.templates.dto.sections.SectionTemplateDto;
+import ru.nabokovsg.templates.dto.sections.NewSectionTemplateDto;
+import ru.nabokovsg.templates.dto.sections.UpdateSectionTemplateDto;
 import ru.nabokovsg.templates.services.SectionTemplateService;
 
 import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+import java.util.List;
 
 @RestController
 @RequestMapping(
-        value = "/template/report/section",
+        value = "/template/section",
         consumes = MediaType.ALL_VALUE,
         produces = MediaType.APPLICATION_JSON_VALUE)
 @RequiredArgsConstructor
 @Validated
-@Tag(name="Шаблон раздела отчета",
-        description="API для работы с данными шаблона раздела отчета")
+@Tag(name="Шаблон раздела",
+        description="API для работы с данными шаблона раздела")
 public class SectionTemplateController {
 
     private final SectionTemplateService service;
 
-    @Operation(summary = "Добавление нового шаблона шаблона")
+    @Operation(summary = "Добавление нового шаблона раздела")
     @PostMapping
-    public ResponseEntity<ReportTemplateDto> save(
+    public ResponseEntity<List<SectionTemplateDto>> save(
                                             @RequestBody @Valid
-                                            @Parameter(description = "Данные нового шаблона раздела отчета")
-                                            NewReportSectionTemplateDto sectionTemplateDto) {
-        return ResponseEntity.ok().body(service.save(sectionTemplateDto));
+                                            @Parameter(description = "Данные нового шаблона раздела")
+                                            NewSectionTemplateDto sectionsDto) {
+        return ResponseEntity.ok().body(service.save(sectionsDto));
     }
 
+    @Operation(summary = "Изменение данных шаблона раздела")
+    @PatchMapping
+    public ResponseEntity<List<SectionTemplateDto>> update(
+            @RequestBody @Valid
+            @Parameter(description = "Данные шаблона раздела")
+            UpdateSectionTemplateDto sectionsDto) {
+        return ResponseEntity.ok().body(service.update(sectionsDto));
+    }
 
+    @Operation(summary = "Получить заголовок по типу документа")
+    @GetMapping("/{reportingDocumentId}")
+    public ResponseEntity<List<SectionTemplateDto>> get(@PathVariable @NotNull @Positive Long reportingDocumentId) {
+        return ResponseEntity.ok().body(service.get(reportingDocumentId));
+    }
 }
