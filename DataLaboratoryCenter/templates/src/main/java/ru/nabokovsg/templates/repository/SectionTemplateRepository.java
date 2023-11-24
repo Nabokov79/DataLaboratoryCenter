d2f35fbad269b752b6602206fe961e2a68266108
NabokovSG <nabokovsg@yandex.ru>
2023-11-24 15:58:51 +0300

    add findAllByReportingDocumentId()

--- DataLaboratoryCenter/templates/src/main/java/ru/nabokovsg/templates/repository/SectionTemplateRepository.java
+++ DataLaboratoryCenter/templates/src/main/java/ru/nabokovsg/templates/repository/SectionTemplateRepository.java
@@ -3,5 +3,9 @@ package ru.nabokovsg.templates.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.nabokovsg.templates.models.SectionTemplate;
 
+import java.util.Set;
+
 public interface SectionTemplateRepository extends JpaRepository<SectionTemplate, Long> {
+
+    Set<SectionTemplate> findAllByReportingDocumentId(Long reportingDocumentId);
 }
