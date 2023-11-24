04679286886f61b571ea3302c244d077e7ec6678
NabokovSG <nabokovsg@yandex.ru>
2023-11-24 15:57:16 +0300

    add get()

--- DataLaboratoryCenter/templates/src/main/java/ru/nabokovsg/templates/services/PageTitleTemplateService.java
+++ DataLaboratoryCenter/templates/src/main/java/ru/nabokovsg/templates/services/PageTitleTemplateService.java
@@ -1,12 +1,18 @@
 package ru.nabokovsg.templates.services;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import ru.nabokovsg.templates.dto.pageTitle.NewPageTitleTemplateDto;
 import ru.nabokovsg.templates.dto.pageTitle.PageTitleTemplateDto;
 import ru.nabokovsg.templates.dto.pageTitle.UpdatePageTitleTemplateDto;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
 
 public interface PageTitleTemplateService {
 
    PageTitleTemplateDto save(NewPageTitleTemplateDto pageTitleDto);
 
    PageTitleTemplateDto update(UpdatePageTitleTemplateDto pageTitleDto);
+
+   PageTitleTemplateDto get(Long reportingDocumentId);
 }
