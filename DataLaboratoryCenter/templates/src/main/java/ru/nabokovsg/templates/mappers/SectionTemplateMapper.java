aaf501731a441b0542b3778ab836dab94b92ad99
NabokovSG <nabokovsg@yandex.ru>
2023-11-29 12:20:10 +0300

    update mapping methods

--- DataLaboratoryCenter/templates/src/main/java/ru/nabokovsg/templates/mappers/SectionTemplateMapper.java
+++ DataLaboratoryCenter/templates/src/main/java/ru/nabokovsg/templates/mappers/SectionTemplateMapper.java
@@ -1,18 +1,25 @@
 package ru.nabokovsg.templates.mappers;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
 import ru.nabokovsg.templates.dto.sections.NewSectionTemplateDataDto;
 import ru.nabokovsg.templates.dto.sections.SectionTemplateDto;
-import ru.nabokovsg.templates.dto.sections.UpdateSectionTemplateDataDto;
+import ru.nabokovsg.templates.dto.sections.UpdateSectionTemplateDto;
 import ru.nabokovsg.templates.models.SectionTemplate;
-import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface SectionTemplateMapper {
 
-   List<SectionTemplate> mapToNewSectionTemplate(List<NewSectionTemplateDataDto> sectionsData);
+   @Mapping(source = "objectTypeId", target = "objectTypeId")
+   @Mapping(source = "reportingDocumentId", target = "reportingDocumentId")
+   @Mapping(source = "sectionData.sequentialNumber", target = "sequentialNumber")
+   @Mapping(source = "sectionData.sectionName", target = "sectionName")
+   SectionTemplate mapToNewSectionTemplate(Long objectTypeId, Long reportingDocumentId, NewSectionTemplateDataDto sectionData);
 
-   SectionTemplate mapToUpdateSectionTemplate(UpdateSectionTemplateDataDto sectionData);
+   @Mapping(source = "sectionDto.sequentialNumber", target = "sequentialNumber")
+   @Mapping(source = "sectionDto.sectionName", target = "sectionName")
+   SectionTemplate mapToUpdateSectionTemplate(@MappingTarget SectionTemplate section, UpdateSectionTemplateDto sectionDto);
 
    SectionTemplateDto mapToSectionTemplateDto(SectionTemplate sectionDto);
 }
