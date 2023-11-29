2708e51d2a3ea632e09696db6ba0fb2f4e26279a
NabokovSG <nabokovsg@yandex.ru>
2023-11-29 12:17:36 +0300

    update all methods

--- DataLaboratoryCenter/templates/src/main/java/ru/nabokovsg/templates/services/PageTitleTemplateService.java
+++ DataLaboratoryCenter/templates/src/main/java/ru/nabokovsg/templates/services/PageTitleTemplateService.java
@@ -1,18 +1,17 @@
 package ru.nabokovsg.templates.services;
 
-import org.springframework.web.bind.annotation.PathVariable;
 import ru.nabokovsg.templates.dto.pageTitle.NewPageTitleTemplateDto;
 import ru.nabokovsg.templates.dto.pageTitle.PageTitleTemplateDto;
 import ru.nabokovsg.templates.dto.pageTitle.UpdatePageTitleTemplateDto;
-
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Positive;
+import java.util.List;
 
 public interface PageTitleTemplateService {
 
-   PageTitleTemplateDto save(NewPageTitleTemplateDto pageTitleDto);
+   List<PageTitleTemplateDto> save(NewPageTitleTemplateDto pageTitleDto);
 
    PageTitleTemplateDto update(UpdatePageTitleTemplateDto pageTitleDto);
 
-   PageTitleTemplateDto get(Long reportingDocumentId);
+   PageTitleTemplateDto get(Long id);
+
+   List<PageTitleTemplateDto> getAll(Long reportingDocumentId);
 }
