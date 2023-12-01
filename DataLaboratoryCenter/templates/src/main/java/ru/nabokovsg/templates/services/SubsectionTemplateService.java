4ea5aab7ac2eea517d6ed06b94b1c694fb6e16b1
NabokovSG <nabokovsg@yandex.ru>
2023-12-01 16:00:24 +0300

    add methods getAll(), addSubsectionTemplateData

--- DataLaboratoryCenter/templates/src/main/java/ru/nabokovsg/templates/services/SubsectionTemplateService.java
+++ DataLaboratoryCenter/templates/src/main/java/ru/nabokovsg/templates/services/SubsectionTemplateService.java
@@ -3,10 +3,9 @@ package ru.nabokovsg.templates.services;
 import ru.nabokovsg.templates.dto.subsection.NewSubsectionTemplateDto;
 import ru.nabokovsg.templates.dto.subsection.SubsectionTemplateDto;
 import ru.nabokovsg.templates.dto.subsection.UpdateSubsectionTemplateDto;
-import ru.nabokovsg.templates.models.SubsectionTemplate;
+import ru.nabokovsg.templates.models.SubsectionTemplateData;
 
 import java.util.List;
-import java.util.Set;
 
 public interface SubsectionTemplateService {
 
@@ -14,5 +13,7 @@ public interface SubsectionTemplateService {
 
     List<SubsectionTemplateDto> update(UpdateSubsectionTemplateDto subsectionsDto);
 
-   Set<SubsectionTemplate> getAllForSectionTemplate(Long sectionId);
+    List<SubsectionTemplateDto> getAll(Long sectionId);
+
+    void addSubsectionTemplateData(Long subsectionId, List<SubsectionTemplateData> subsectionTemplateData);
 }
