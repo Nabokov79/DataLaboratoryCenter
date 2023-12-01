fd796b5ab52d313034333a4dfcf5eee739863fd5
NabokovSG <nabokovsg@yandex.ru>
2023-12-01 15:53:17 +0300

    add fields

--- DataLaboratoryCenter/templates/src/main/java/ru/nabokovsg/templates/dto/subsection/subsectionData/NewSubsectionTemplateDataDto.java
+++ DataLaboratoryCenter/templates/src/main/java/ru/nabokovsg/templates/dto/subsection/subsectionData/NewSubsectionTemplateDataDto.java
@@ -7,6 +7,8 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
 
 @Setter
 @Getter
@@ -15,15 +17,29 @@ import javax.validation.constraints.NotBlank;
 @Schema(description = "Данные нового подраздела")
 public class NewSubsectionTemplateDataDto {
 
-    @Schema(description = "Тип данных подраздела")
-    @NotBlank(message = "data type subsection should not be blank")
+    @Schema(description = "Индентификатор подраздела")
+    @NotNull(message = "subsection id should not be null")
+    @Positive(message = "subsection id can only be positive")
+    private Long subsectionId;
+    @Schema(description = "Тип данных")
+    @NotBlank(message = "Subsection data type should not be blank")
     private String subsectionDataType;
+    @Schema(description = "Указать методическую документацию")
+    @NotNull(message = "Methodological document should not be null")
+    private Boolean methodologicalDocument;
+    @Schema(description = "Указать нормативную документацию")
+    @NotNull(message = "Regulatory document should not be null")
+    private Boolean regulatoryDocument;
     @Schema(description = "Идентификатор типа объекта")
     private Long objectTypeId;
     @Schema(description = "Индентификатор структурного подразделения организации")
     private Long divisionId;
-    @Schema(description = "Вид структурного подразделения организации")
-    private String divisionType;
     @Schema(description = "Пользовательское название структурного подразделения организации")
     private String divisionName;
+    @Schema(description = "Указать адресс структурного подразделения")
+    private Boolean divisionAddress;
+    @Schema(description = "Указать контакты структурного подразделения")
+    private Boolean divisionContact;
+    @Schema(description = "Указать лицензию/аттестацию структурного подразделения")
+    private String divisionLicense;
 }
