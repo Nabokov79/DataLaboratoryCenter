23406e713f873fda467077f6eeaa97495e0eb316
NabokovSG <nabokovsg@yandex.ru>
2023-12-01 15:57:03 +0300

    add

--- DataLaboratoryCenter/templates/src/main/java/ru/nabokovsg/templates/dto/subsection/subsectionData/SubsectionDataDto.java
+++ DataLaboratoryCenter/templates/src/main/java/ru/nabokovsg/templates/dto/subsection/subsectionData/SubsectionDataDto.java
@@ -1,23 +1,19 @@
 package ru.nabokovsg.templates.dto.subsection.subsectionData;
 
-import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Setter
 @Getter
-@NoArgsConstructor
+@AllArgsConstructor
 public class SubsectionDataDto {
 
-    @Schema(description = "Тип данных подраздела")
     private String subsectionDataType;
-    @Schema(description = "Идентификатор типа объекта")
+    private Boolean methodologicalDocument;
+    private Boolean regulatoryDocument;
     private Long objectTypeId;
-    @Schema(description = "Индентификатор структурного подразделения организации")
     private Long divisionId;
-    @Schema(description = "Вид структурного подразделения организации")
     private String divisionType;
-    @Schema(description = "Пользовательское название структурного подразделения организации")
     private String divisionName;
 }
