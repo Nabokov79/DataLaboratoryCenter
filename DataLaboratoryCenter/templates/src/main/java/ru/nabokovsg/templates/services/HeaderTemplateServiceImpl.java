77c4d3559c46e862f310f315177b1d824b0bf276
NabokovSG <nabokovsg@yandex.ru>
2023-12-01 15:52:58 +0300

    replace methods: setOrganizationData to OrganizationDataService; setBranchData to BranchDataService;  setDepartmentData to DepartmentDataService

--- DataLaboratoryCenter/templates/src/main/java/ru/nabokovsg/templates/services/HeaderTemplateServiceImpl.java
+++ DataLaboratoryCenter/templates/src/main/java/ru/nabokovsg/templates/services/HeaderTemplateServiceImpl.java
@@ -13,10 +13,10 @@ import ru.nabokovsg.templates.dto.header.UpdateHeaderTemplateDto;
 import ru.nabokovsg.templates.exceptions.NotFoundException;
 import ru.nabokovsg.templates.mappers.HeaderTemplateMapper;
 import ru.nabokovsg.templates.models.HeaderTemplate;
-import ru.nabokovsg.templates.models.builder.TemplateData;
-import ru.nabokovsg.templates.models.enums.DataType;
 import ru.nabokovsg.templates.repository.HeaderTemplateRepository;
-import ru.nabokovsg.templates.services.factory.StringFactory;
+import ru.nabokovsg.templates.services.dataHandlers.BranchDataService;
+import ru.nabokovsg.templates.services.dataHandlers.DepartmentDataService;
+import ru.nabokovsg.templates.services.dataHandlers.OrganizationDataService;
 
 import java.util.*;
 import java.util.stream.Collectors;
@@ -28,8 +28,10 @@ public class HeaderTemplateServiceImpl implements HeaderTemplateService {
 
     private final HeaderTemplateRepository repository;
     private final HeaderTemplateMapper mapper;
-    private final StringFactory factory;
     private final TemplateClient client;
+    private final OrganizationDataService organizationDataService;
+    private final BranchDataService branchDataService;
+    private final DepartmentDataService departmentDataService;
 
     @Override
     public List<HeaderTemplateDto> save(NewHeaderTemplateDto headerDto) {
@@ -37,7 +39,8 @@ public class HeaderTemplateServiceImpl implements HeaderTemplateService {
          headerDto.getReportingDocumentIds()).stream()
                                              .collect(Collectors.toMap(HeaderTemplate::getReportingDocumentId, h -> h));
         HeaderTemplate headerData = set(new HeaderTemplate(), mapper.mapToNewHeaderTemplateDataDto(headerDto));
-        List<HeaderTemplate> headers =  repository.saveAll(headerDto.getReportingDocumentIds().stream()
+        List<HeaderTemplate> headers =  repository.saveAll(headerDto.getReportingDocumentIds()
+                                                                        .stream()
                                                                         .map(id -> {
                                                                             HeaderTemplate header = headersDb.get(id);
                                                                             if (header == null) {
@@ -50,14 +53,16 @@ public class HeaderTemplateServiceImpl implements HeaderTemplateService {
         if (!headersDb.isEmpty()) {
             headers = Stream.of(headersDb.values(), headers).flatMap(Collection::stream).toList();
         }
-        return headers.stream().map(mapper::mapToHeaderTemplateDto).toList();
+        return headers.stream()
+                      .map(mapper::mapToHeaderTemplateDto)
+                      .toList();
     }
 
     @Override
     public HeaderTemplateDto update(UpdateHeaderTemplateDto headerDto) {
         return mapper.mapToHeaderTemplateDto(repository.save(
                 set(repository.findById(headerDto.getId()).orElseThrow(() -> new NotFoundException(
-                                                String.format("HeaderTemplate with id=%s not found", headerDto.getId())))
+                                               String.format("HeaderTemplate with id=%s not found", headerDto.getId())))
                   , mapper.mapToUpdateHeaderTemplateDataDto(headerDto))
         ));
     }
@@ -70,90 +75,23 @@ public class HeaderTemplateServiceImpl implements HeaderTemplateService {
         );
     }
 
-    private HeaderTemplate set(HeaderTemplate header, HeaderTemplateDataDto data) {
-        OrganizationDto organization = client.getOrganization(data.getOrganizationId());
-        setOrganizationData(header, organization, data);
-        setBranchData(header, organization, data);
-        setDepartmentData(header, organization, data);
+    private HeaderTemplate set(HeaderTemplate header, HeaderTemplateDataDto headerDto) {
+        OrganizationDto organization = client.getOrganization(headerDto.getOrganizationId());
+        organizationDataService.getHeaderData(header, organization, headerDto);
+        branchDataService.getHeaderData(header
+                           , organization.getBranches()
+                            .stream()
+                            .collect(Collectors.toMap(BranchDto::getId, b -> b))
+                            .get(headerDto.getBranchId())
+                          , headerDto);
+        departmentDataService.getHeaderData(header
+                                , organization.getBranches()
+                                 .stream()
+                                 .map(BranchDto::getDepartments)
+                                 .flatMap(Collection::stream)
+                                 .collect(Collectors.toMap(DepartmentDto::getId, d -> d))
+                                 .get(headerDto.getDepartmentId())
+                                , headerDto);
         return header;
-    }
-
-    private void setOrganizationData(HeaderTemplate header, OrganizationDto organization, HeaderTemplateDataDto data) {
-        if (data.getOrganizationFullName()) {
-            header.setOrganization(organization.getOrganization());
-        } else {
-            header.setOrganization(organization.getShortNameOrganization());
-        }
-        if (data.getOrganizationLicense()) {
-            header.setOrganizationLicense(factory.create(new TemplateData.Builder()
-                                                                         .type(DataType.LICENSE)
-                                                                         .licenses(organization.getLicenses())
-                                                                         .build()));
-        }
-        if (data.getOrganizationContacts()) {
-            header.setOrganizationContacts(factory.create(new TemplateData.Builder()
-                                                                            .type(DataType.CONTACT)
-                                                                            .contact(organization.getContact())
-                                                                            .build()));
-        }
-    }
-
-    private void setBranchData(HeaderTemplate header, OrganizationDto organization, HeaderTemplateDataDto data) {
-        BranchDto branch = organization.getBranches()
-                .stream()
-                .collect(Collectors.toMap(BranchDto::getId, b -> b))
-                .get(data.getBranchId());
-        if (data.getBranchFullName()) {
-            header.setBranch(branch.getBranch());
-        } else {
-            header.setBranch(branch.getShortNameBranch());
-        }
-        if (data.getBranchLicense()) {
-            header.setOrganizationLicense(factory.create(new TemplateData.Builder()
-                                                                         .type(DataType.LICENSE)
-                                                                         .licenses(branch.getLicenses())
-                                                                         .build()));
-        } else {
-            header.setOrganizationLicense(null);
-        }
-        if (data.getBranchContacts()) {
-            header.setBranchContacts(factory.create(new TemplateData.Builder()
-                                                                      .type(DataType.CONTACT)
-                                                                      .contact(branch.getContact())
-                                                                      .build()));
-        } else {
-            header.setBranchContacts(null);
-        }
-    }
-
-    private void setDepartmentData(HeaderTemplate header, OrganizationDto organization, HeaderTemplateDataDto data) {
-        DepartmentDto department = organization.getBranches()
-                                                .stream()
-                                                .map(BranchDto::getDepartments)
-                                                .flatMap(Collection::stream)
-                                                .collect(Collectors.toMap(DepartmentDto::getId, d -> d))
-                                                .get(data.getDepartmentId());
-        if (data.getDepartmentFullName()) {
-            header.setDepartment(department.getDepartment());
-        } else {
-            header.setDepartment(department.getShortNameDepartment());
-        }
-        if (data.getDepartmentLicense()) {
-            header.setDepartmentLicense(factory.create(new TemplateData.Builder()
-                                                                       .type(DataType.LICENSE)
-                                                                       .licenses(department.getLicenses())
-                                                                       .build()));
-        } else {
-            header.setDepartmentLicense(null);
-        }
-        if (data.getDepartmentContacts()) {
-           header.setDepartmentContacts(factory.create(new TemplateData.Builder()
-                                                                         .type(DataType.CONTACT)
-                                                                         .address(department.getAddress())
-                                                                         .contact(department.getContact())
-                                                                         .build()));
-        } else {
-            header.setDepartmentContacts(null);
-        }
     }
 }
