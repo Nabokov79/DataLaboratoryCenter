2870d7bd9903af42e211c836ac2c5d209e2a8dfb
NabokovSG <nabokovsg@yandex.ru>
2023-12-01 15:59:40 +0300

    update methods

--- DataLaboratoryCenter/templates/src/main/java/ru/nabokovsg/templates/services/SubsectionTemplateDataServiceImpl.java
+++ DataLaboratoryCenter/templates/src/main/java/ru/nabokovsg/templates/services/SubsectionTemplateDataServiceImpl.java
@@ -3,20 +3,17 @@ package ru.nabokovsg.templates.services;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import ru.nabokovsg.templates.client.TemplateClient;
-import ru.nabokovsg.templates.client.dto.ObjectsTypeDto;
-import ru.nabokovsg.templates.dto.subsection.subsectionData.NewSubsectionTemplateDataDto;
-import ru.nabokovsg.templates.dto.subsection.subsectionData.SubsectionDataDto;
-import ru.nabokovsg.templates.dto.subsection.subsectionData.UpdateSubsectionTemplateDataDto;
+import ru.nabokovsg.templates.client.dto.DocumentationDto;
+import ru.nabokovsg.templates.dto.subsection.subsectionData.*;
 import ru.nabokovsg.templates.exceptions.BadRequestException;
 import ru.nabokovsg.templates.mappers.SubsectionDataTemplateMapper;
 import ru.nabokovsg.templates.models.SubsectionTemplateData;
-import ru.nabokovsg.templates.models.enums.DivisionType;
 import ru.nabokovsg.templates.models.enums.SubsectionDataType;
 import ru.nabokovsg.templates.repository.SubsectionTemplateDataRepository;
-import ru.nabokovsg.templates.services.factory.SubsectionDataFactory;
+import ru.nabokovsg.templates.services.builders.*;
+import ru.nabokovsg.templates.services.converters.ConverterToEnum;
 
 import java.util.*;
-import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -24,85 +21,84 @@ public class SubsectionTemplateDataServiceImpl implements SubsectionTemplateData
 
     private final SubsectionTemplateDataRepository repository;
     private final SubsectionDataTemplateMapper mapper;
-    private final SubsectionDataFactory factory;
     private final TemplateClient client;
+    private final ConverterToEnum converter;
+    private final StringBuilderService stringBuilder;
+    private final SubsectionTemplateService subsectionService;
 
     @Override
-    public List<SubsectionTemplateData> save(List<NewSubsectionTemplateDataDto> subsectionsData) {
-        List<SubsectionTemplateData> subsectionData = new ArrayList<>();
-        for (NewSubsectionTemplateDataDto data : subsectionsData) {
-            subsectionData.addAll(getData(mapper.mapToSubsectionDataForNew(data)));
+    public List<SubsectionTemplateDataDto> save(NewSubsectionTemplateDataDto subsectionData) {
+        SubsectionDataType type = converter.convertToSubsectionDataType(subsectionData.getSubsectionDataType());
+        Set<SubsectionTemplateData> subsectionDataDb =
+                          repository.findAllBySubsectionIdAndSubsectionDataType(subsectionData.getSubsectionId(), type);
+        if (subsectionDataDb.isEmpty()) {
+            List<SubsectionTemplateData> subsectionTemplateData = repository.saveAll(
+                    getDataByType(type, mapper.mapToNewSubsectionDataDto(subsectionData)));
+            subsectionService.addSubsectionTemplateData(subsectionData.getSubsectionId(), subsectionTemplateData);
+            return mapper.mapToSubsectionDataDto(subsectionTemplateData);
         }
-        Set<SubsectionTemplateData> subsectionDataDb = repository.findAllBySubsectionData(
-                subsectionData.stream()
-                        .map(SubsectionTemplateData::getSubsectionData)
-                        .toList());
-        subsectionData = filter(subsectionData, subsectionDataDb.stream()
-                .map(SubsectionTemplateData::getSubsectionData)
-                .collect(Collectors.toSet()));
-        if (!subsectionData.isEmpty()) {
-            if (subsectionDataDb.isEmpty()) {
-                return repository.saveAll(subsectionData);
-            } else {
-                subsectionDataDb.addAll(repository.saveAll(subsectionData));
-            }
-        }
-        return new ArrayList<>(subsectionDataDb);
+        return mapper.mapToSubsectionDataDto(subsectionDataDb.stream().toList());
     }
 
     @Override
-    public List<SubsectionTemplateData> update(UpdateSubsectionTemplateDataDto subsectionData) {
-        List<Long> ids = repository.findAllIdSubsectionDataType(subsectionData.getSubsectionDataType());
-        List<SubsectionTemplateData> subsectionsData = getData(mapper.mapToSubsectionDataForUpdate(subsectionData));
+    public List<SubsectionTemplateDataDto> update(UpdateSubsectionTemplateDataDto subsectionData) {
+        SubsectionDataType type = converter.convertToSubsectionDataType(subsectionData.getSubsectionDataType());
+        List<Long> ids = repository.findAllIdSubsectionDataType(
+                converter.convertToSubsectionDataType(subsectionData.getSubsectionDataType())
+        );
+        List<SubsectionTemplateData> subsectionsData = getDataByType(type
+                                                                 , mapper.mapToUpdateSubsectionDataDto(subsectionData));
         List<SubsectionTemplateData> subsectionsDataDb = new ArrayList<>();
         for (int index=0; index < subsectionsData.size(); index++) {
             SubsectionTemplateData data = subsectionsData.get(index);
             data.setId(ids.get(index));
             subsectionsDataDb.add(data);
         }
-         return repository.saveAll(subsectionsDataDb);
+         return mapper.mapToSubsectionDataDto(repository.saveAll(subsectionsDataDb));
     }
 
-    private List<SubsectionTemplateData> getData(SubsectionDataDto subsectionData) {
-        SubsectionDataType type = convertToSubsectionDataType(subsectionData.getSubsectionDataType());
+    private List<SubsectionTemplateData> getDataByType(SubsectionDataType type, SubsectionDataDto subsectionData) {
         switch (type) {
-            case ALL_DOCUMENT, REGULATORY_DOCUMENT, METHODOLOGICAL_DOCUMENT ->
-            { ObjectsTypeDto objectType = client.getObjectsType(subsectionData.getObjectTypeId());
-                return factory.createByDocumentationData(type, objectType.getDocumentations())
-                        .stream()
-                        .peek(s -> s.setSubsectionDataType(String.valueOf(type)))
-                        .toList();
+            case ORGANIZATION -> {
+                return List.of(mapper.mapForDivisionData(type
+                          , stringBuilder.convertOrganization(client.getOrganization(subsectionData.getDivisionId()))));
             }
-            case LABORATORY_DATA ->
-            { return factory.createByDivisionData(convertToDivisionType(subsectionData.getDivisionType())
-                            , subsectionData.getDivisionId()
-                            , subsectionData.getDivisionName())
-                    .stream()
-                    .peek(s -> s.setSubsectionDataType(String.valueOf(type)))
-                    .toList();
+            case BRANCH -> {
+                return List.of(mapper.mapForDivisionData(type
+                                                        , stringBuilder.convertBranch(subsectionData.getDivisionName()
+                                                        , client.getBranch(subsectionData.getDivisionId()))));
+            }
+            case DEPARTMENT -> {
+                return List.of(mapper.mapForDivisionData(type
+                                                      , stringBuilder.convertDepartment(subsectionData.getDivisionName()
+                                                      , client.getDepartment(subsectionData.getDivisionId()))));
+            }
+            case ALL_DOCUMENT, REGULATORY_DOCUMENT, METHODOLOGICAL_DOCUMENT -> {
+                return filterDocumentsData(client.getObjectsType(subsectionData.getObjectTypeId()).getDocumentations()
+                                                            , subsectionData.getMethodologicalDocument()
+                                                            , subsectionData.getRegulatoryDocument())
+                                      .stream()
+                                      .map(d -> mapper.mapToSubsectionData(type, subsectionData.getObjectTypeId(), stringBuilder.convertDocumentation(d)))
+                        .peek(d -> d.setObjectTypeId(subsectionData.getObjectTypeId()))
+                                      .toList();
             }
             default -> throw new BadRequestException(String.format("SubsectionData type=%s is not supported", type));
         }
     }
 
-    private List<SubsectionTemplateData> filter(List<SubsectionTemplateData> subsectionsData
-            , Set<String> subsectionData) {
-        return subsectionsData.stream()
-                .filter(s -> !subsectionData.contains(s.getSubsectionData()))
-                .toList();
-    }
-
-    private SubsectionDataType convertToSubsectionDataType(String subsectionDataType) {
-        return SubsectionDataType.from(subsectionDataType)
-                .orElseThrow(() -> new BadRequestException(
-                        String.format("Unknown subsection data type=%s",subsectionDataType))
-                );
-    }
-
-    private DivisionType convertToDivisionType(String divisionType) {
-        return DivisionType.from(divisionType)
-                .orElseThrow(() -> new BadRequestException(
-                        String.format("Unknown subsection data type=%s", divisionType))
-                );
+    private List<DocumentationDto> filterDocumentsData(List<DocumentationDto> documentation
+                                                    ,  Boolean methodologicalDocument
+                                                    ,  Boolean regulatoryDocument) {
+        if (methodologicalDocument) {
+            return documentation.stream()
+                    .filter(d -> d.getMethodologicalDocument().equals(true))
+                    .toList();
+        }
+        if (regulatoryDocument) {
+            return documentation.stream()
+                    .filter(d -> d.getRegulatoryDocument().equals(true))
+                    .toList();
+        }
+        return documentation;
     }
 }
