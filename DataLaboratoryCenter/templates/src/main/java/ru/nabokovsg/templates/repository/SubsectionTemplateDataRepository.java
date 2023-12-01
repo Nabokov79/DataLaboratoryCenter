71ef13a94fd5592b32579fcbbe5a0063b3884df6
NabokovSG <nabokovsg@yandex.ru>
2023-12-01 15:59:13 +0300

    update methods

--- DataLaboratoryCenter/templates/src/main/java/ru/nabokovsg/templates/repository/SubsectionTemplateDataRepository.java
+++ DataLaboratoryCenter/templates/src/main/java/ru/nabokovsg/templates/repository/SubsectionTemplateDataRepository.java
@@ -2,17 +2,17 @@ package ru.nabokovsg.templates.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import ru.nabokovsg.templates.models.SubsectionTemplateData;
+import ru.nabokovsg.templates.models.enums.SubsectionDataType;
 
 import java.util.List;
 import java.util.Set;
 
 public interface SubsectionTemplateDataRepository extends JpaRepository<SubsectionTemplateData, Long> {
 
-    @Query("select s from SubsectionTemplateData s where s.subsectionData in :subsectionsData")
-    Set<SubsectionTemplateData> findAllBySubsectionData(@Param("subsectionsData") List<String> subsectionsData);
+    Set<SubsectionTemplateData> findAllBySubsectionIdAndSubsectionDataType(Long subsectionId
+                                                                         , SubsectionDataType subsectionDataType);
 
     @Query("select s.id from SubsectionTemplateData s where s.subsectionDataType =?1")
-    List<Long> findAllIdSubsectionDataType(String subsectionsDataType);
+    List<Long> findAllIdSubsectionDataType(SubsectionDataType subsectionsDataType);
 }
