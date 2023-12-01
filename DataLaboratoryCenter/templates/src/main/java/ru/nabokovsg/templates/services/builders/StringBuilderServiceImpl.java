389c05afeead8ec01334aa5da3c452de24937516
NabokovSG <nabokovsg@yandex.ru>
2023-12-01 15:56:47 +0300

    delete methods create

--- DataLaboratoryCenter/templates/src/main/java/ru/nabokovsg/templates/services/builders/StringBuilderServiceImpl.java
+++ DataLaboratoryCenter/templates/src/main/java/ru/nabokovsg/templates/services/builders/StringBuilderServiceImpl.java
@@ -1,9 +1,7 @@
-package ru.nabokovsg.templates.services.factory;
+package ru.nabokovsg.templates.services.builders;
 
 import org.springframework.stereotype.Service;
 import ru.nabokovsg.templates.client.dto.*;
-import ru.nabokovsg.templates.exceptions.BadRequestException;
-import ru.nabokovsg.templates.models.builder.TemplateData;
 
 import java.time.LocalDate;
 import java.util.Comparator;
@@ -11,23 +9,10 @@ import java.util.List;
 import java.util.NoSuchElementException;
 
 @Service
-public class StringFactoryImpl implements StringFactory {
+public class StringBuilderServiceImpl implements StringBuilderService {
 
     @Override
-    public String create(TemplateData data) {
-        switch (data.getType()) {
-            case LICENSE -> {return  convertLicense(data.getLicenses());}
-            case CONTACT -> {return convertContacts(data.getContact(), data.getAddress());}
-            case SIGNATURE -> {return convertEmployee(data.getEmployee());}
-            case ORGANIZATION -> { return convertOrganization(data.getOrganization());}
-            case BRANCH -> {return convertBranch(data.getDivisionName(), data.getBranch());}
-            case DEPARTMENT -> {return convertDepartment(data.getDivisionName(), data.getDepartment());}
-            case DOCUMENTATION -> {return convertDocumentation(data.getDocument());}
-            default -> throw new BadRequestException(String.format("data type=%s is not supported", data.getType()));
-        }
-    }
-
-    private String convertDocumentation(DocumentationDto documentations) {
+    public String convertDocumentation(DocumentationDto documentations) {
         String string = String.join("", "«", documentations.getTitle(), "»");
         if (documentations.getNumber() != null) {
             string = String.join(" ", documentations.getNumber(), string);
@@ -38,7 +23,8 @@ public class StringFactoryImpl implements StringFactory {
         return string;
     }
 
-    private String convertLicense(List<LicenseDto> licenses) {
+    @Override
+    public String convertLicense(List<LicenseDto> licenses) {
         LicenseDto license = licenses.stream()
                 .filter(l -> l.getEndData().isAfter(LocalDate.now()))
                 .toList()
@@ -55,18 +41,21 @@ public class StringFactoryImpl implements StringFactory {
                                                   license.getStartData().toString());
     }
 
-    private String convertContacts(ContactDto contact, AddressDto address) {
+    @Override
+    public String convertContacts(ContactDto contact, AddressDto address) {
         return String.join("//"
         , String.join(", ", String.valueOf(contact.getIndex()), convertAddress(address))
         , String.join(" ","тел./факс", contact.getPhone(),"/", contact.getFax())
         , String.join(" ", "E-mail:", contact.getEmail()));
     }
 
-    private String convertToShortRequisites(ContactDto contact, AddressDto address) {
+    @Override
+    public String convertToShortRequisites(ContactDto contact, AddressDto address) {
         return String.join(", ", String.valueOf(contact.getIndex()),  convertAddress(address));
     }
 
-    private String convertAddress(AddressDto address) {
+    @Override
+    public String convertAddress(AddressDto address) {
         String string = String.join(", ", address.getCity()
                                                 , String.join(" ", address.getStreet()
                                                                      , "д.", String.valueOf(address.getHouseNumber())));
@@ -80,7 +69,8 @@ public class StringFactoryImpl implements StringFactory {
         return string;
     }
 
-    private String convertEmployee(ShortEmployeeDto employee) {
+    @Override
+    public String convertEmployee(ShortEmployeeDto employee) {
         return String.join("/"
                 , employee.getPost()
                 , String.join(". ", String.join(".", String.valueOf(employee.getName().charAt(0))
@@ -89,13 +79,15 @@ public class StringFactoryImpl implements StringFactory {
                                          , employee.getSurname()));
     }
 
-    private String convertOrganization(OrganizationDto organization) {
+    @Override
+    public String convertOrganization(OrganizationDto organization) {
         return String.join(". ", organization.getOrganization()
                 , convertToShortRequisites(organization.getContact(), organization.getAddress())
                 , convertLicense(organization.getLicenses()));
     }
 
-    private String convertBranch(String divisionName, BranchDto branch) {
+    @Override
+    public String convertBranch(String divisionName, BranchDto branch) {
         String name = branch.getBranch();
         if (divisionName != null) {
             name = divisionName;
@@ -105,7 +97,8 @@ public class StringFactoryImpl implements StringFactory {
                 , convertLicense(branch.getLicenses()));
     }
 
-    private String convertDepartment(String divisionName, DepartmentDto department) {
+    @Override
+    public String convertDepartment(String divisionName, DepartmentDto department) {
         String name = department.getDepartment();
         if (divisionName != null) {
             name = divisionName;
