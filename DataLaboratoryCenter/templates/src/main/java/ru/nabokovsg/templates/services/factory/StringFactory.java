dd1b5b3d49fac2778edbc5d3058019500b8325f0
NabokovSG <nabokovsg@yandex.ru>
2023-12-01 15:50:56 +0300

    delete

--- DataLaboratoryCenter/templates/src/main/java/ru/nabokovsg/templates/services/factory/StringFactory.java
+++
@@ -1,8 +0,0 @@
-package ru.nabokovsg.templates.services.factory;
-
-import ru.nabokovsg.templates.models.builder.TemplateData;
-
-public interface StringFactory {
-
-    String create(TemplateData data);
-}
