86f25d108a87e902694b9624297ee4957d2e62e7
Nabokov79 <nabokovsg@yandex.ru>
2023-12-10 19:32:43 +0300

    delete

--- DataLaboratoryCenter/templates/src/main/java/ru/nabokovsg/templates/services/ReportProtocolTemplateService.java
+++
@@ -1,9 +0,0 @@
-package ru.nabokovsg.templates.services;
-
-import ru.nabokovsg.templates.dto.reportProtocol.NewReportProtocolTemplateDto;
-import ru.nabokovsg.templates.dto.reportProtocol.ReportProtocolTemplateDto;
-
-public interface ReportProtocolTemplateService {
-
-    ReportProtocolTemplateDto save(NewReportProtocolTemplateDto protocolDto);
-}
