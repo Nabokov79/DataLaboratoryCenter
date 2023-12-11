24ee6a8bdcccb210d06f3801e7a6fe1499c0015a
NabokovSG <nabokovsg@yandex.ru>
2023-12-11 12:32:23 +0300

    add fields

--- DataLaboratoryCenter/templates/src/main/java/ru/nabokovsg/templates/dto/table/NewTableTemplateDto.java
+++ DataLaboratoryCenter/templates/src/main/java/ru/nabokovsg/templates/dto/table/NewTableTemplateDto.java
@@ -1,4 +1,47 @@
 package ru.nabokovsg.templates.dto.table;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import ru.nabokovsg.templates.dto.columns.NewColumnHeaderTemplateDto;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+import java.util.List;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Schema(description = "Данные шаблона новой таблицы")
 public class NewTableTemplateDto {
+
+    @Schema(description = "Индентификатор типа отчетного документа")
+    @NotNull(message = "reporting document id should not be null")
+    @Positive(message = "reporting document id can only be positive")
+    private Long reportingDocumentId;
+    @Schema(description = "Индентификатор типа объекта")
+    @NotNull(message = "object type id should not be null")
+    @Positive(message = "object type id can only be positive")
+    private Long objectTypeId;
+    @Schema(description = "Индентификатор организации")
+    @NotBlank(message = "organization id should not be blank")
+    private String tableDataType;
+    @Schema(description = "Порядковый номер таблицы")
+    private Integer sequentialNumber;
+    @Schema(description = "Название таблицы")
+    private String tableName;
+    @Schema(description = "Текст перед таблицей")
+    private String textBeforeTable;
+    @Schema(description = "Текст после таблицы")
+    private String textAfterTable;
+    @Schema(description = "Шаблоны колонок таблицы")
+    @NotNull(message = "column headers should not be null")
+    @NotEmpty(message = "column headers should not be empty")
+    private List<@Valid NewColumnHeaderTemplateDto> columnHeaders;
 }
