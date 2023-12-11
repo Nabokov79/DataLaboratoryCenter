26e90d19bae20b0db652714d844121fb1d45180f
NabokovSG <nabokovsg@yandex.ru>
2023-12-11 12:31:57 +0300

    add mappers

--- DataLaboratoryCenter/templates/src/main/java/ru/nabokovsg/templates/mappers/TableTemplateMapper.java
+++ DataLaboratoryCenter/templates/src/main/java/ru/nabokovsg/templates/mappers/TableTemplateMapper.java
@@ -1,7 +1,38 @@
 package ru.nabokovsg.templates.mappers;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
+import ru.nabokovsg.templates.dto.table.NewTableTemplateDto;
+import ru.nabokovsg.templates.dto.table.TableTemplateDto;
+import ru.nabokovsg.templates.dto.table.UpdateTableTemplateDto;
+import ru.nabokovsg.templates.models.ColumnHeaderTemplate;
+import ru.nabokovsg.templates.models.TableTemplate;
+
+import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface TableTemplateMapper {
+
+    @Mapping(source = "tableDto.tableDataType", target = "tableDataType")
+    @Mapping(source = "tableDto.objectTypeId", target = "objectTypeId")
+    @Mapping(source = "tableDto.reportingDocumentId", target = "reportingDocumentId")
+    @Mapping(source = "tableDto.sequentialNumber", target = "sequentialNumber")
+    @Mapping(source = "tableDto.tableName", target = "tableName")
+    @Mapping(source = "tableDto.textBeforeTable", target = "textBeforeTable")
+    @Mapping(source = "tableDto.textAfterTable", target = "textAfterTable")
+    @Mapping(source = "columnHeaders", target = "columnHeaders")
+    TableTemplate mapToNewTableTemplate(NewTableTemplateDto tableDto, List<ColumnHeaderTemplate> columnHeaders);
+
+    @Mapping(source = "tableDto.tableDataType", target = "tableDataType")
+    @Mapping(source = "tableDto.sequentialNumber", target = "sequentialNumber")
+    @Mapping(source = "tableDto.tableName", target = "tableName")
+    @Mapping(source = "tableDto.textBeforeTable", target = "textBeforeTable")
+    @Mapping(source = "tableDto.textAfterTable", target = "textAfterTable")
+    @Mapping(source = "columnHeaders", target = "columnHeaders")
+    TableTemplate mapToUpdateTableTemplate(@MappingTarget TableTemplate table
+                                                        , UpdateTableTemplateDto tableDto
+                                                        , List<ColumnHeaderTemplate> columnHeaders);
+
+    TableTemplateDto mapToTableTemplateDto(TableTemplate table);
 }
