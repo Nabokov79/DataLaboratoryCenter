2cc06d4abd4288bd41dbb57ab0e3317588f98b5c
NabokovSG <nabokovsg@yandex.ru>
2023-12-13 16:03:47 +0300

    add method getByTableDataType

--- DataLaboratoryCenter/templates/src/main/java/ru/nabokovsg/templates/services/TableTemplateService.java
+++ DataLaboratoryCenter/templates/src/main/java/ru/nabokovsg/templates/services/TableTemplateService.java
@@ -3,10 +3,13 @@ package ru.nabokovsg.templates.services;
 import ru.nabokovsg.templates.dto.table.NewTableTemplateDto;
 import ru.nabokovsg.templates.dto.table.TableTemplateDto;
 import ru.nabokovsg.templates.dto.table.UpdateTableTemplateDto;
+import ru.nabokovsg.templates.models.TableTemplate;
 
 public interface TableTemplateService {
 
     TableTemplateDto save(NewTableTemplateDto tableDto);
 
     TableTemplateDto update(UpdateTableTemplateDto tableDto);
+
+    TableTemplate getByTableDataType(String tableDataType);
 }
