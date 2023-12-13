473ad5bbe69a5ba66dde31c1f4192dd6fd7d2020
NabokovSG <nabokovsg@yandex.ru>
2023-12-13 16:08:52 +0300

    delete validate for fields index, fax

--- DataLaboratoryCenter/data/src/main/java/ru/nabokovsg/data/dto/contact/NewContactDto.java
+++ DataLaboratoryCenter/data/src/main/java/ru/nabokovsg/data/dto/contact/NewContactDto.java
@@ -8,8 +8,6 @@ import lombok.Setter;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Positive;
 
 @Setter
 @Getter
@@ -19,14 +17,11 @@ import javax.validation.constraints.Positive;
 public class NewContactDto {
 
     @Schema(description = "Почтовый индекс")
-    @NotNull(message = "index should not be blank")
-    @Positive(message = "index must be positive")
     private Integer index;
     @Schema(description = "Номер телефона")
     @NotBlank(message = "phone should not be blank")
     private String phone;
     @Schema(description = "Факс")
-    @NotBlank(message = "fax should not be blank")
     private String fax;
     @Schema(description = "электронная почта")
     @NotBlank(message = "email should not be blank")
