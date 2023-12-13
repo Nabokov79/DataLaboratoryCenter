886ba9e0c36bd9d3dc1ecb20f4447a3ed1aa0fb8
NabokovSG <nabokovsg@yandex.ru>
2023-12-13 16:05:19 +0300

    add methods: save, update, delete

--- DataLaboratoryCenter/templates/src/main/java/ru/nabokovsg/templates/services/SubsectionTemplateService.java
+++ DataLaboratoryCenter/templates/src/main/java/ru/nabokovsg/templates/services/SubsectionTemplateService.java
@@ -1,4 +1,14 @@
 package ru.nabokovsg.templates.services;
 
+import ru.nabokovsg.templates.dto.subsection.NewSubsectionTemplateDto;
+import ru.nabokovsg.templates.dto.subsection.SubsectionTemplateDto;
+import ru.nabokovsg.templates.dto.subsection.UpdateSubsectionTemplateDto;
+
 public interface SubsectionTemplateService {
+
+    SubsectionTemplateDto save(NewSubsectionTemplateDto subsectionsDto);
+
+    SubsectionTemplateDto update(UpdateSubsectionTemplateDto subsectionsDto);
+
+    void delete(Long id);
 }
--- DataLaboratoryCenter/templates/src/main/java/ru/nabokovsg/templates/services/SubsectionTemplateServiceImpl.java
+++ DataLaboratoryCenter/templates/src/main/java/ru/nabokovsg/templates/services/SubsectionTemplateServiceImpl.java
@@ -2,6 +2,12 @@ package ru.nabokovsg.templates.services;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import ru.nabokovsg.templates.dto.subsection.NewSubsectionTemplateDto;
+import ru.nabokovsg.templates.dto.subsection.SubsectionTemplateDto;
+import ru.nabokovsg.templates.dto.subsection.UpdateSubsectionTemplateDto;
+import ru.nabokovsg.templates.exceptions.NotFoundException;
+import ru.nabokovsg.templates.mappers.SubsectionTemplateMapper;
+import ru.nabokovsg.templates.models.SubsectionTemplate;
 import ru.nabokovsg.templates.repository.SubsectionTemplateRepository;
 
 @Service
@@ -9,4 +15,47 @@ import ru.nabokovsg.templates.repository.SubsectionTemplateRepository;
 public class SubsectionTemplateServiceImpl implements SubsectionTemplateService {
 
     private final SubsectionTemplateRepository repository;
+    private final SubsectionTemplateMapper mapper;
+    private final  TableTemplateService tableService;
+    private final SubsectionDataTemplateService subsectionDataService;
+
+    @Override
+    public SubsectionTemplateDto save(NewSubsectionTemplateDto subsectionsDto) {
+        SubsectionTemplate subsection = mapper.mapToNewSubsectionTemplate(subsectionsDto);
+        if (subsectionsDto.getTableDataType() != null) {
+            subsection.setTable(tableService.getByTableDataType(subsectionsDto.getTableDataType()));
+
+        }
+        if (!subsectionsDto.getSubsectionDataIds().isEmpty()) {
+            subsection.setSubsectionData(subsectionDataService.getAllById(subsectionsDto.getSubsectionDataIds()));
+        }
+        return mapper.mapToSubsectionTemplateDto(repository.save(subsection));
+    }
+
+    @Override
+    public SubsectionTemplateDto update(UpdateSubsectionTemplateDto subsectionsDto) {
+        if (repository.existsById(subsectionsDto.getId())) {
+            SubsectionTemplate subsection = mapper.mapToUpdateSubsectionTemplate(subsectionsDto);
+            if (subsectionsDto.getTableDataType() != null) {
+                subsection.setTable(tableService.getByTableDataType(subsectionsDto.getTableDataType()));
+
+            }
+            if (!subsectionsDto.getSubsectionDataIds().isEmpty()) {
+                subsection.setSubsectionData(subsectionDataService.getAllById(subsectionsDto.getSubsectionDataIds()));
+            }
+            return mapper.mapToSubsectionTemplateDto(repository.save(subsection));
+        }
+        throw new NotFoundException(
+                String.format("Subsection template with id=%s not found for update", subsectionsDto.getId())
+        );
+    }
+
+    @Override
+    public void delete(Long id) {
+        if (repository.existsById(id)) {
+            repository.deleteById(id);
+            return;
+        }
+        throw new NotFoundException(String.format("SubsectionTemplate with id=%s not found for delete", id));
+    }
 }
