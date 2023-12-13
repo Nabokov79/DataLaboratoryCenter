3ef9e3164953edfda4f6654f70426743e93dd872
NabokovSG <nabokovsg@yandex.ru>
2023-12-13 16:09:51 +0300

    delete methods convertToProtocolType

--- DataLaboratoryCenter/templates/src/main/java/ru/nabokovsg/templates/services/converter/ConverterStringToEnumService.java
+++ DataLaboratoryCenter/templates/src/main/java/ru/nabokovsg/templates/services/converter/ConverterStringToEnumService.java
@@ -1,17 +1,14 @@
 package ru.nabokovsg.templates.services.converter;
 
 import ru.nabokovsg.templates.models.enums.ColumnDataType;
-import ru.nabokovsg.templates.models.enums.ProtocolType;
-import ru.nabokovsg.templates.models.enums.SubsectionDataType;
+import ru.nabokovsg.templates.models.enums.DivisionType;
 import ru.nabokovsg.templates.models.enums.TableDataType;
 
 public interface ConverterStringToEnumService {
 
     ColumnDataType covertToColumnDataType(String columnDataType);
 
-    SubsectionDataType convertToSubsectionDataType(String subsectionDataType);
+    DivisionType convertToDivisionType(String divisionType);
 
     TableDataType convertToTableDataType(String tableDataType);
-
-    ProtocolType convertToProtocolType(String protocolType);
 }
