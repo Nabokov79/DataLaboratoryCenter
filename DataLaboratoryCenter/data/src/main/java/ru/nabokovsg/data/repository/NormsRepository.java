2b2fbbe1efff8b4573fd81a25e7db3f37b34a153
Nabokov79 <nabokovsg@yandex.ru>
2023-12-17 18:01:39 +0300

    delete

--- DataLaboratoryCenter/data/src/main/java/ru/nabokovsg/data/repository/NormsRepository.java
+++
@@ -1,7 +0,0 @@
-package ru.nabokovsg.data.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import ru.nabokovsg.data.models.Norm;
-
-public interface NormsRepository extends JpaRepository<Norm, Long> {
-}
