5a9a141d14c2aab123d47eaeae3f24b4ee30c7fc
NabokovSG <nabokovsg@yandex.ru>
2023-12-22 15:33:57 +0300

    add

--- DataLaboratoryCenter/data/src/main/java/ru/nabokovsg/data/services/ReportingDocumentService.java
+++ DataLaboratoryCenter/data/src/main/java/ru/nabokovsg/data/services/ReportingDocumentService.java
@@ -3,14 +3,15 @@ package ru.nabokovsg.data.services;
 import ru.nabokovsg.data.dto.reportingDocument.NewReportingDocumentDto;
 import ru.nabokovsg.data.dto.reportingDocument.ReportingDocumentDto;
 import ru.nabokovsg.data.dto.reportingDocument.UpdateReportingDocumentDto;
+import ru.nabokovsg.data.models.enums.DocumentType;
 
 import java.util.List;
 
 public interface ReportingDocumentService {
 
-    List<ReportingDocumentDto> save(List<NewReportingDocumentDto> reportingDocumentDto);
+    List<ReportingDocumentDto> save(DocumentType documentType, List<NewReportingDocumentDto> documentDto);
 
-    List<ReportingDocumentDto> update(List<UpdateReportingDocumentDto> reportingDocumentDto);
+    List<ReportingDocumentDto> update(List<UpdateReportingDocumentDto> documentDto);
 
     ReportingDocumentDto get(Long id);
 
