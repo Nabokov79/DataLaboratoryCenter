7690736a906980330110b36b205e4d3e9ce9aafe
Nabokov79 <nabokovsg@yandex.ru>
2023-12-24 21:30:12 +0300

    add methods: get(), delete(), existsById(), saveWithTable()

--- DataLaboratoryCenter/templates/src/main/java/ru/nabokovsg/templates/services/SubsectionTemplateServiceImpl.java
+++ DataLaboratoryCenter/templates/src/main/java/ru/nabokovsg/templates/services/SubsectionTemplateServiceImpl.java
@@ -8,6 +8,7 @@ import ru.nabokovsg.templates.dto.subsection.UpdateSubsectionTemplateDto;
 import ru.nabokovsg.templates.exceptions.NotFoundException;
 import ru.nabokovsg.templates.mappers.SubsectionTemplateMapper;
 import ru.nabokovsg.templates.models.SubsectionTemplate;
+import ru.nabokovsg.templates.models.TableTemplate;
 import ru.nabokovsg.templates.repository.SubsectionTemplateRepository;
 
 @Service
@@ -16,30 +17,46 @@ public class SubsectionTemplateServiceImpl implements SubsectionTemplateService 
 
     private final SubsectionTemplateRepository repository;
     private final SubsectionTemplateMapper mapper;
-    private final  TableTemplateService tableService;
     private final SubsectionDataTemplateService subsectionDataService;
+    private final SectionTemplateService sectionService;
+    private final ProtocolTemplateService protocolService;
 
     @Override
-    public SubsectionTemplateDto save(NewSubsectionTemplateDto subsectionsDto) {
-        SubsectionTemplate subsection = mapper.mapToNewSubsectionTemplate(subsectionsDto);
-        if (subsectionsDto.getTableDataType() != null) {
-            subsection.setTable(tableService.getByTableDataType(subsectionsDto.getTableDataType()));
+    public SubsectionTemplateDto saveFromSectionTemplate(Long sectionId, NewSubsectionTemplateDto subsectionDto) {
+        if (sectionService.existsById(sectionId)) {
+            SubsectionTemplate subsection = mapper.mapToNewSubsectionTemplate(subsectionDto);
+            if (!subsectionDto.getSubsectionDataIds().isEmpty()) {
+                subsection = mapper.mapSubsectionData(subsection
+                                              , subsectionDataService.getAllById(subsectionDto.getSubsectionDataIds()));
+            }
+            subsection = repository.save(subsection);
+            sectionService.saveWithSubsectionTemplate(sectionId, subsection);
+            return mapper.mapToSubsectionTemplateDto(subsection);
+        }
+        throw new NotFoundException(
+                String.format("Section template with id=%s not found for add subsection template", sectionId)
+        );
+    }
 
+    @Override
+    public SubsectionTemplateDto saveFromProtocolTemplate(Long protocolId, NewSubsectionTemplateDto subsectionDto) {
+        if (protocolService.existsById(protocolId)) {
+            SubsectionTemplate subsection = mapper.mapToNewSubsectionTemplate(subsectionDto);
+            if (!subsectionDto.getSubsectionDataIds().isEmpty()) {
+                subsection = mapper.mapSubsectionData(subsection
+                                              , subsectionDataService.getAllById(subsectionDto.getSubsectionDataIds()));
+            }
+            return mapper.mapToSubsectionTemplateDto(repository.save(subsection));
         }
-        if (!subsectionsDto.getSubsectionDataIds().isEmpty()) {
-            subsection.setSubsectionData(subsectionDataService.getAllById(subsectionsDto.getSubsectionDataIds()));
-        }
-        return mapper.mapToSubsectionTemplateDto(repository.save(subsection));
+        throw new NotFoundException(
+                String.format("Protocol template with id=%s not found for add subsection template", protocolId)
+        );
     }
 
     @Override
     public SubsectionTemplateDto update(UpdateSubsectionTemplateDto subsectionsDto) {
         if (repository.existsById(subsectionsDto.getId())) {
             SubsectionTemplate subsection = mapper.mapToUpdateSubsectionTemplate(subsectionsDto);
-            if (subsectionsDto.getTableDataType() != null) {
-                subsection.setTable(tableService.getByTableDataType(subsectionsDto.getTableDataType()));
-
-            }
             if (!subsectionsDto.getSubsectionDataIds().isEmpty()) {
                 subsection.setSubsectionData(subsectionDataService.getAllById(subsectionsDto.getSubsectionDataIds()));
             }
@@ -51,11 +68,32 @@ public class SubsectionTemplateServiceImpl implements SubsectionTemplateService 
     }
 
     @Override
+    public SubsectionTemplateDto get(Long id) {
+        return mapper.mapToSubsectionTemplateDto(getById(id));
+    }
+
+    @Override
     public void delete(Long id) {
         if (repository.existsById(id)) {
             repository.deleteById(id);
             return;
         }
         throw new NotFoundException(String.format("SubsectionTemplate with id=%s not found for delete", id));
+    }
+
+    @Override
+    public boolean existsById(Long id) {
+        return repository.existsById(id);
+    }
+
+    @Override
+    public void saveWithTable(Long subsectionId, TableTemplate table) {
+        repository.save(mapper.mapTableTemplate(getById(subsectionId), table));
+    }
+
+    public SubsectionTemplate getById(Long id) {
+        return repository.findById(id)
+                .orElseThrow(() -> new NotFoundException(String.format("Subsection template with id=%s not found", id))
+                );
     }
 }
