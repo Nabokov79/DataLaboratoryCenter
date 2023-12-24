1c7993230ef99e0ed0a84c95589e4661ece50eb9
Nabokov79 <nabokovsg@yandex.ru>
2023-12-24 21:28:15 +0300

    add

--- DataLaboratoryCenter/templates/src/main/java/ru/nabokovsg/templates/services/SubsectionDataTemplateService.java
+++ DataLaboratoryCenter/templates/src/main/java/ru/nabokovsg/templates/services/SubsectionDataTemplateService.java
@@ -3,15 +3,16 @@ package ru.nabokovsg.templates.services;
 import ru.nabokovsg.templates.dto.subsectionDada.*;
 import ru.nabokovsg.templates.models.SubsectionDataTemplate;
 
+import javax.validation.Valid;
 import java.util.List;
 
 public interface SubsectionDataTemplateService {
 
-    SubsectionDataTemplateDto saveDivisionData(NewDivisionDataDto divisionDataDto);
+    SubsectionDataTemplateDto saveDivisionData(@Valid NewDivisionDataDto divisionDataDto);
 
-    List<SubsectionDataTemplateDto> saveDocumentationData(NewDocumentationDataDto documentationDataDto);
+    List<SubsectionDataTemplateDto> saveDocumentationData(@Valid NewDocumentationDataDto documentationDataDto);
 
-    List<SubsectionDataTemplateDto> saveMeasuringToolData(List<NewMeasuringToolDataDto> measuringToolDataDto);
+    List<SubsectionDataTemplateDto> saveMeasuringToolData(@Valid List<NewMeasuringToolDataDto> measuringToolDataDto);
 
     List<SubsectionDataTemplate> getAllById(List<Long> ids);
 }
