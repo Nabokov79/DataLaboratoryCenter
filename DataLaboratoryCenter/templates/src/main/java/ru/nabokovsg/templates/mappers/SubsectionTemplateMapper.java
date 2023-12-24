35c94207bbd6da723af54c066b95e3271e08848e
Nabokov79 <nabokovsg@yandex.ru>
2023-12-24 21:31:31 +0300

    update mapping

--- DataLaboratoryCenter/templates/src/main/java/ru/nabokovsg/templates/mappers/SubsectionTemplateMapper.java
+++ DataLaboratoryCenter/templates/src/main/java/ru/nabokovsg/templates/mappers/SubsectionTemplateMapper.java
@@ -1,10 +1,16 @@
 package ru.nabokovsg.templates.mappers;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
 import ru.nabokovsg.templates.dto.subsection.NewSubsectionTemplateDto;
 import ru.nabokovsg.templates.dto.subsection.SubsectionTemplateDto;
 import ru.nabokovsg.templates.dto.subsection.UpdateSubsectionTemplateDto;
+import ru.nabokovsg.templates.models.SubsectionDataTemplate;
 import ru.nabokovsg.templates.models.SubsectionTemplate;
+import ru.nabokovsg.templates.models.TableTemplate;
+
+import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface SubsectionTemplateMapper {
@@ -14,4 +20,11 @@ public interface SubsectionTemplateMapper {
     SubsectionTemplate mapToUpdateSubsectionTemplate(UpdateSubsectionTemplateDto subsectionsDto);
 
     SubsectionTemplateDto mapToSubsectionTemplateDto(SubsectionTemplate subsection);
+
+    @Mapping(source = "subsectionData", target = "subsectionData")
+    SubsectionTemplate mapSubsectionData(@MappingTarget SubsectionTemplate subsection
+                                                      , List<SubsectionDataTemplate> subsectionData);
+
+    @Mapping(source = "table", target = "table")
+    SubsectionTemplate mapTableTemplate(@MappingTarget SubsectionTemplate subsection, TableTemplate table);
 }
--- DataLaboratoryCenter/templates/src/main/java/ru/nabokovsg/templates/mappers/TableTemplateMapper.java
+++ DataLaboratoryCenter/templates/src/main/java/ru/nabokovsg/templates/mappers/TableTemplateMapper.java
@@ -2,8 +2,8 @@ package ru.nabokovsg.templates.mappers;
 
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-import org.mapstruct.MappingTarget;
 import ru.nabokovsg.templates.dto.table.NewTableTemplateDto;
+import ru.nabokovsg.templates.dto.table.ShortTableTemplateDto;
 import ru.nabokovsg.templates.dto.table.TableTemplateDto;
 import ru.nabokovsg.templates.dto.table.UpdateTableTemplateDto;
 import ru.nabokovsg.templates.models.ColumnHeaderTemplate;
@@ -14,21 +14,19 @@ import java.util.List;
 @Mapper(componentModel = "spring")
 public interface TableTemplateMapper {
 
-    @Mapping(source = "tableDto.tableDataType", target = "tableDataType")
-    @Mapping(source = "tableDto.objectTypeId", target = "objectTypeId")
-    @Mapping(source = "tableDto.reportingDocumentId", target = "reportingDocumentId")
-    @Mapping(source = "tableDto.sequentialNumber", target = "sequentialNumber")
-    @Mapping(source = "tableDto.tableName", target = "tableName")
+    @Mapping(source = "tableDataType", target = "tableDataType")
+    @Mapping(source = "sequentialNumber", target = "sequentialNumber")
+    @Mapping(source = "tableName", target = "tableName")
     @Mapping(source = "columnHeaders", target = "columnHeaders")
+    @Mapping(target = "id", ignore = true)
     TableTemplate mapToNewTableTemplate(NewTableTemplateDto tableDto, List<ColumnHeaderTemplate> columnHeaders);
 
-    @Mapping(source = "tableDto.tableDataType", target = "tableDataType")
-    @Mapping(source = "tableDto.sequentialNumber", target = "sequentialNumber")
-    @Mapping(source = "tableDto.tableName", target = "tableName")
+    @Mapping(source = "tableDataType", target = "tableDataType")
+    @Mapping(source = "sequentialNumber", target = "sequentialNumber")
+    @Mapping(source = "tableName", target = "tableName")
     @Mapping(source = "columnHeaders", target = "columnHeaders")
-    TableTemplate mapToUpdateTableTemplate(@MappingTarget TableTemplate table
-                                                        , UpdateTableTemplateDto tableDto
-                                                        , List<ColumnHeaderTemplate> columnHeaders);
+    @Mapping(source = "id", target = "id")
+    TableTemplate mapToUpdateTableTemplate(UpdateTableTemplateDto tableDto , List<ColumnHeaderTemplate> columnHeaders);
 
     TableTemplateDto mapToTableTemplateDto(TableTemplate table);
 }
