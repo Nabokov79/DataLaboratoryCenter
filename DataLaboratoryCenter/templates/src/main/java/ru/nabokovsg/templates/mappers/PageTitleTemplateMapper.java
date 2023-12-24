8d06209f53437c63c81acd0b6072b330721b982b
Nabokov79 <nabokovsg@yandex.ru>
2023-12-24 21:25:00 +0300

    add

--- DataLaboratoryCenter/templates/src/main/java/ru/nabokovsg/templates/mappers/PageTitleTemplateMapper.java
+++ DataLaboratoryCenter/templates/src/main/java/ru/nabokovsg/templates/mappers/PageTitleTemplateMapper.java
@@ -2,40 +2,35 @@ package ru.nabokovsg.templates.mappers;
 
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-import org.mapstruct.Mappings;
 import ru.nabokovsg.templates.client.dto.ReportingDocumentDto;
-import ru.nabokovsg.templates.dto.header.HeaderTemplateDto;
 import ru.nabokovsg.templates.dto.pageTitle.NewPageTitleTemplateDto;
 import ru.nabokovsg.templates.dto.pageTitle.PageTitleTemplateDto;
+import ru.nabokovsg.templates.models.HeaderTemplate;
 import ru.nabokovsg.templates.models.PageTitleTemplate;
 
 @Mapper(componentModel = "spring")
 public interface PageTitleTemplateMapper {
 
-    @Mappings({
-            @Mapping(source = "header", target = "header"),
-            @Mapping(source = "reportingDocument.document", target = "title"),
-            @Mapping(source = "reportingDocument.documentTitle", target = "heading"),
-            @Mapping(source = "signature", target = "signature"),
-            @Mapping(source = "year", target = "year"),
-            @Mapping(target = "id", ignore = true)
-    })
+    @Mapping(source = "headerDb", target = "header")
+    @Mapping(source = "reportingDocument.name", target = "title")
+    @Mapping(source = "reportingDocument.title", target = "heading")
+    @Mapping(source = "signature", target = "signature")
+    @Mapping(source = "year", target = "year")
+    @Mapping(target = "id", ignore = true)
     PageTitleTemplate mapToNewPageTitleTemplate(NewPageTitleTemplateDto pageTitleDto
-                                              , HeaderTemplateDto header
+                                              , HeaderTemplate headerDb
                                               , ReportingDocumentDto reportingDocument
                                               , String signature
                                               , String year);
 
-    @Mappings({
-            @Mapping(source = "header", target = "header"),
-            @Mapping(source = "reportingDocument.document", target = "title"),
-            @Mapping(source = "reportingDocument.documentTitle", target = "heading"),
-            @Mapping(source = "signature", target = "signature"),
-            @Mapping(source = "year", target = "year"),
-            @Mapping(source = "id", target = "id")
-    })
+    @Mapping(source = "headerDb", target = "header")
+    @Mapping(source = "reportingDocument.name", target = "title")
+    @Mapping(source = "reportingDocument.title", target = "heading")
+    @Mapping(source = "signature", target = "signature")
+    @Mapping(source = "year", target = "year")
+    @Mapping(source = "id", target = "id")
     PageTitleTemplate mapToUpdatePageTitleTemplate(Long id
-                                                 , HeaderTemplateDto header
+                                                 , HeaderTemplate headerDb
                                                  , ReportingDocumentDto reportingDocument
                                                  , String signature
                                                  , String year);
