a103ad0f83195973e287449c0ee9ec61c9f5869d
Nabokov79 <nabokovsg@yandex.ru>
2023-12-25 19:55:20 +0300

    delete methods: mapSubsectionData(), mapTableTemplate()

--- DataLaboratoryCenter/templates/src/main/java/ru/nabokovsg/templates/mappers/SubsectionTemplateMapper.java
+++ DataLaboratoryCenter/templates/src/main/java/ru/nabokovsg/templates/mappers/SubsectionTemplateMapper.java
@@ -1,16 +1,10 @@
 package ru.nabokovsg.templates.mappers;
 
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
-import org.mapstruct.MappingTarget;
 import ru.nabokovsg.templates.dto.subsection.NewSubsectionTemplateDto;
 import ru.nabokovsg.templates.dto.subsection.SubsectionTemplateDto;
 import ru.nabokovsg.templates.dto.subsection.UpdateSubsectionTemplateDto;
-import ru.nabokovsg.templates.models.SubsectionDataTemplate;
 import ru.nabokovsg.templates.models.SubsectionTemplate;
-import ru.nabokovsg.templates.models.TableTemplate;
-
-import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface SubsectionTemplateMapper {
@@ -20,11 +14,4 @@ public interface SubsectionTemplateMapper {
     SubsectionTemplate mapToUpdateSubsectionTemplate(UpdateSubsectionTemplateDto subsectionsDto);
 
     SubsectionTemplateDto mapToSubsectionTemplateDto(SubsectionTemplate subsection);
-
-    @Mapping(source = "subsectionData", target = "subsectionData")
-    SubsectionTemplate mapSubsectionData(@MappingTarget SubsectionTemplate subsection
-                                                      , List<SubsectionDataTemplate> subsectionData);
-
-    @Mapping(source = "table", target = "table")
-    SubsectionTemplate mapTableTemplate(@MappingTarget SubsectionTemplate subsection, TableTemplate table);
 }
