2db7dcd330bb4a78a90c87e01f96790023841267
Nabokov79 <nabokovsg@yandex.ru>
2023-12-25 19:54:37 +0300

    delete method mapForSectionTemplate()

--- DataLaboratoryCenter/templates/src/main/java/ru/nabokovsg/templates/mappers/ReportTemplateMapper.java
+++ DataLaboratoryCenter/templates/src/main/java/ru/nabokovsg/templates/mappers/ReportTemplateMapper.java
@@ -2,14 +2,11 @@ package ru.nabokovsg.templates.mappers;
 
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-import org.mapstruct.MappingTarget;
 import ru.nabokovsg.templates.dto.pageTitle.ShortPageTitleTemplateDto;
 import ru.nabokovsg.templates.dto.section.ShortSectionTemplateDto;
 import ru.nabokovsg.templates.models.PageTitleTemplate;
 import ru.nabokovsg.templates.models.ReportTemplate;
 import ru.nabokovsg.templates.models.SectionTemplate;
-
-import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface ReportTemplateMapper {
@@ -17,9 +14,6 @@ public interface ReportTemplateMapper {
     @Mapping(source = "pageTitle", target = "pageTitle")
     @Mapping(target = "id", ignore = true)
     ReportTemplate mapToNewReportTemplate(PageTitleTemplate pageTitle);
-
-    @Mapping(source = "sections", target = "sections")
-    ReportTemplate mapForSectionTemplate(@MappingTarget ReportTemplate report, List<SectionTemplate> sections);
 
     ShortSectionTemplateDto mapToShortSectionTemplateDto(SectionTemplate section);
 
