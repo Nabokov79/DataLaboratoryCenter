002000d4c9d9f7e0f53563a2a7b198e78f303ca1
Nabokov79 <nabokovsg@yandex.ru>
2023-12-25 19:55:43 +0300

    update mapping

--- DataLaboratoryCenter/templates/src/main/java/ru/nabokovsg/templates/mappers/TableTemplateMapper.java
+++ DataLaboratoryCenter/templates/src/main/java/ru/nabokovsg/templates/mappers/TableTemplateMapper.java
@@ -3,7 +3,6 @@ package ru.nabokovsg.templates.mappers;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import ru.nabokovsg.templates.dto.table.NewTableTemplateDto;
-import ru.nabokovsg.templates.dto.table.ShortTableTemplateDto;
 import ru.nabokovsg.templates.dto.table.TableTemplateDto;
 import ru.nabokovsg.templates.dto.table.UpdateTableTemplateDto;
 import ru.nabokovsg.templates.models.ColumnHeaderTemplate;
@@ -14,18 +13,18 @@ import java.util.List;
 @Mapper(componentModel = "spring")
 public interface TableTemplateMapper {
 
-    @Mapping(source = "tableDataType", target = "tableDataType")
-    @Mapping(source = "sequentialNumber", target = "sequentialNumber")
-    @Mapping(source = "tableName", target = "tableName")
+    @Mapping(source = "tableDto.tableDataType", target = "tableDataType")
+    @Mapping(source = "tableDto.sequentialNumber", target = "sequentialNumber")
+    @Mapping(source = "tableDto.tableName", target = "tableName")
     @Mapping(source = "columnHeaders", target = "columnHeaders")
     @Mapping(target = "id", ignore = true)
     TableTemplate mapToNewTableTemplate(NewTableTemplateDto tableDto, List<ColumnHeaderTemplate> columnHeaders);
 
-    @Mapping(source = "tableDataType", target = "tableDataType")
-    @Mapping(source = "sequentialNumber", target = "sequentialNumber")
-    @Mapping(source = "tableName", target = "tableName")
+    @Mapping(source = "tableDto.tableDataType", target = "tableDataType")
+    @Mapping(source = "tableDto.sequentialNumber", target = "sequentialNumber")
+    @Mapping(source = "tableDto.tableName", target = "tableName")
     @Mapping(source = "columnHeaders", target = "columnHeaders")
-    @Mapping(source = "id", target = "id")
+    @Mapping(source = "tableDto.id", target = "id")
     TableTemplate mapToUpdateTableTemplate(UpdateTableTemplateDto tableDto , List<ColumnHeaderTemplate> columnHeaders);
 
     TableTemplateDto mapToTableTemplateDto(TableTemplate table);
