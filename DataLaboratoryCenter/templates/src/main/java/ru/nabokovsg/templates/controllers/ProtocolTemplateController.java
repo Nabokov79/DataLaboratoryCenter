82353be7dc0e178342213a7b1102a3f8fb0319a5
NabokovSG <nabokovsg@yandex.ru>
2023-12-27 16:44:37 +0300

    add

--- DataLaboratoryCenter/templates/src/main/java/ru/nabokovsg/templates/controllers/ProtocolTemplateController.java
+++ DataLaboratoryCenter/templates/src/main/java/ru/nabokovsg/templates/controllers/ProtocolTemplateController.java
@@ -1,12 +1,21 @@
 package ru.nabokovsg.templates.controllers;
 
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import ru.nabokovsg.templates.dto.protocol.NewProtocolTemplateDto;
+import ru.nabokovsg.templates.dto.protocol.ProtocolTemplateDto;
+import ru.nabokovsg.templates.dto.protocol.ShortProtocolTemplateDto;
+import ru.nabokovsg.templates.dto.protocol.UpdateProtocolTemplateDto;
 import ru.nabokovsg.templates.services.ProtocolTemplateService;
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+import java.util.List;
 
 @RestController
 @RequestMapping(
@@ -20,4 +29,40 @@ import ru.nabokovsg.templates.services.ProtocolTemplateService;
 public class ProtocolTemplateController {
 
     private final ProtocolTemplateService service;
+
+    @Operation(summary = "Данные шаблона нового протокола/заключения")
+    @PostMapping
+    public ResponseEntity<ShortProtocolTemplateDto> save(@RequestBody @Valid NewProtocolTemplateDto protocolDto) {
+        return ResponseEntity.ok().body(service.save(protocolDto));
+    }
+
+    @Operation(summary = "Данные для изменения информации в протокола/заключения")
+    @PatchMapping
+    public ResponseEntity<ShortProtocolTemplateDto> update(@RequestBody @Valid UpdateProtocolTemplateDto protocolDto) {
+        return ResponseEntity.ok().body(service.update(protocolDto));
+    }
+
+    @Operation(summary = "Получить краткие данные протоколов/заключений")
+    @GetMapping("/all")
+    public ResponseEntity<List<ShortProtocolTemplateDto>> getAll() {
+        return ResponseEntity.ok().body(service.getAll());
+    }
+
+    @Operation(summary = "Добавить шаблон заключения к шаблону протокола/заключения")
+    @GetMapping("/{id}/conclusion")
+    public ResponseEntity<ProtocolTemplateDto> addConclusion(@PathVariable @NotNull @Positive Long id) {
+        return ResponseEntity.ok().body(service.addConclusion(id));
+    }
+
+    @Operation(summary = "Добавить шаблон заключения к шаблону протокола/заключения")
+    @GetMapping("/{id}/appendices")
+    public ResponseEntity<ProtocolTemplateDto> addAppendices(@PathVariable @NotNull @Positive Long id) {
+        return ResponseEntity.ok().body(service.addAppendices(id));
+    }
+
+    @Operation(summary = "Добавить шаблон заключения к шаблону протокола/заключения")
+    @GetMapping("/{id}/recommendation")
+    public ResponseEntity<ProtocolTemplateDto> addRecommendation(@PathVariable @NotNull @Positive Long id) {
+        return ResponseEntity.ok().body(service.addRecommendation(id));
+    }
 }
