2d9c7a72f2c02fc8027e2244355510870ef1914d
NabokovSG <nabokovsg@yandex.ru>
2023-12-27 16:44:05 +0300

    add method findAllByReportingDocumentId()

--- DataLaboratoryCenter/templates/src/main/java/ru/nabokovsg/templates/repository/PageTitleTemplateRepository.java
+++ DataLaboratoryCenter/templates/src/main/java/ru/nabokovsg/templates/repository/PageTitleTemplateRepository.java
@@ -3,11 +3,7 @@ package ru.nabokovsg.templates.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.nabokovsg.templates.models.PageTitleTemplate;
 
-import java.util.Set;
-
 public interface PageTitleTemplateRepository extends JpaRepository<PageTitleTemplate, Long> {
 
     PageTitleTemplate findByObjectTypeIdAndReportingDocumentId(Long objectTypeId, Long reportingDocumentId);
-
-    Set<PageTitleTemplate> findAllByReportingDocumentId(Long reportingDocumentId);
 }
