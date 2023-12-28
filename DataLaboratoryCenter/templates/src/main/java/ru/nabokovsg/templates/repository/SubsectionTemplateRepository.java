7945c7931ec4e78ff80057516587c258927b2e57
NabokovSG <nabokovsg@yandex.ru>
2023-12-28 16:52:56 +0300

    add methods: findBySubsectionNameAndSectionId(), findBySubsectionNameAndProtocolId

--- DataLaboratoryCenter/templates/src/main/java/ru/nabokovsg/templates/repository/SubsectionTemplateRepository.java
+++ DataLaboratoryCenter/templates/src/main/java/ru/nabokovsg/templates/repository/SubsectionTemplateRepository.java
@@ -1,7 +1,14 @@
 package ru.nabokovsg.templates.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import ru.nabokovsg.templates.models.SubsectionTemplate;
 
 public interface SubsectionTemplateRepository extends JpaRepository<SubsectionTemplate, Long> {
+
+    @Query("select s from SubsectionTemplate s where s.subsectionName = ?1 and SectionTemplate.id = ?2")
+    SubsectionTemplate findBySubsectionNameAndSectionId(String subsectionName, Long sectionId);
+
+    @Query("select s from SubsectionTemplate s where s.subsectionName = ?1 and ProtocolTemplate.id = ?2")
+    SubsectionTemplate findBySubsectionNameAndProtocolId(String subsectionName, Long protocolId);
 }
