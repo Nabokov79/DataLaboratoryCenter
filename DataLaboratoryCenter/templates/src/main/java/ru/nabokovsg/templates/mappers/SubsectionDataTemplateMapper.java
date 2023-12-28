965f7bfb028d8c796c9b74a46863a111694ea27e
NabokovSG <nabokovsg@yandex.ru>
2023-12-28 16:52:06 +0300

    add

--- DataLaboratoryCenter/templates/src/main/java/ru/nabokovsg/templates/mappers/SubsectionDataTemplateMapper.java
+++ DataLaboratoryCenter/templates/src/main/java/ru/nabokovsg/templates/mappers/SubsectionDataTemplateMapper.java
@@ -3,15 +3,12 @@ package ru.nabokovsg.templates.mappers;
 import org.mapstruct.Mapper;
 import ru.nabokovsg.templates.dto.subsectionDada.DivisionDataParam;
 import ru.nabokovsg.templates.dto.subsectionDada.NewDivisionDataDto;
-import ru.nabokovsg.templates.dto.subsectionDada.SubsectionDataTemplateDto;
 import ru.nabokovsg.templates.models.SubsectionDataTemplate;
 
 @Mapper(componentModel = "spring")
 public interface SubsectionDataTemplateMapper {
 
     SubsectionDataTemplate mapToSubsectionDataTemplate(String valueData);
-
-    SubsectionDataTemplateDto mapToSubsectionDataTemplateDto(SubsectionDataTemplate subsectionData);
 
     DivisionDataParam mapToDivisionDataParam(NewDivisionDataDto divisionDataDto);
 }
