ee9167e1ef4a637c9a7290db8730e9303a71a5c1
NabokovSG <nabokovsg@yandex.ru>
2023-12-28 16:50:08 +0300

    add

--- DataLaboratoryCenter/templates/src/main/java/ru/nabokovsg/templates/dto/subsection/NewSubsectionTemplateDto.java
+++ DataLaboratoryCenter/templates/src/main/java/ru/nabokovsg/templates/dto/subsection/NewSubsectionTemplateDto.java
@@ -2,6 +2,10 @@ package ru.nabokovsg.templates.dto.subsection;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.*;
+import ru.nabokovsg.templates.dto.subsectionDada.NewDivisionDataDto;
+import ru.nabokovsg.templates.dto.subsectionDada.NewDocumentationDataDto;
+import ru.nabokovsg.templates.dto.subsectionDada.NewMeasuringToolDataDto;
+
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Positive;
@@ -14,7 +18,7 @@ import java.util.List;
 @Schema(description = "Данные нового подраздела")
 public class NewSubsectionTemplateDto {
 
-    @Schema(description = "Тип данных подраздела")
+    @Schema(description = "Название подраздела")
     @NotBlank(message = "subsection name should not be blank")
     private String subsectionName;
     @Schema(description = "Порядковый номер подраздела")
@@ -26,6 +30,10 @@ public class NewSubsectionTemplateDto {
     @Schema(description = "Показать номер подраздела в документе")
     @NotNull(message = "subsection number should not be null")
     private boolean subsectionNumber;
-    @Schema(description = "Индентификаторы данных подраздела")
-    private List<Long> subsectionDataIds;
+    @Schema(description = "Данные структурного подразделения")
+    private NewDivisionDataDto division;
+    @Schema(description = "Данные нормативно-технической документации")
+    private NewDocumentationDataDto documentation;
+    @Schema(description = "Данные средст контроля и измерений")
+    private List<NewMeasuringToolDataDto> measuringTools;
 }
